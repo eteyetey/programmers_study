aa6ea5dd4bc0e5e5b9a29af0ac3743352ba8510b
eteyetey <67794444+eteyetey@users.noreply.github.com>
2026-05-11 12:10:18 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        
+        int pizza = 1;
+        while(!(slice*pizza>=n)){
+            pizza++;
+        }
+        return pizza;
+    }
+}
