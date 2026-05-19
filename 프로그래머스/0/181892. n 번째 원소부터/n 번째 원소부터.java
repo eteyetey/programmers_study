b6cf0800f462e6150be4727911826a3aca84f756
eteyetey <67794444+eteyetey@users.noreply.github.com>
2026-05-19 09:40:18 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,6 +1,10 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        return Arrays.copyOfRange(num_list,n-1,num_list.length);
+        int arr[] = new int[num_list.length-n+1];
+        for(int i=0;i<num_list.length-n+1;i++){
+            arr[i] = num_list[n+i-1];
+        }
+        return arr;
     }
 }
