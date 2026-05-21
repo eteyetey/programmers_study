b6d85e524955c0caee2ccd825d03a01d8e838c91
eteyetey <67794444+eteyetey@users.noreply.github.com>
2026-05-21 21:55:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 11.44 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = Arrays.copyOf(arr,arr.length);
+        for(int i =0;i<answer.length;i++){
+            if(arr[i]>=50&&arr[i]%2==0){
+                answer[i]/=2;
+            }else if(arr[i]<50&&arr[i]%2!=0){
+                answer[i]*=2;
+            }
+        }
+        return answer;
+    }
+}
